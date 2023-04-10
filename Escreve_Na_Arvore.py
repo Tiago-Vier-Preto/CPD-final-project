10d3cfe10461bfa13430b7c1630e592e4f0e5853
Thiago Trautwein Santos <ttrautweinsantos@gmail.com>
2023-04-10 17:37:45 -0300

    update escreve_na_arvore

--- Escreve_Na_Arvore.py
+++ Escreve_Na_Arvore.py
@@ -6,14 +6,12 @@ arq = open("database.bin", "rb")
 tree = bplus_tree()
 tree.open("./")
 
-i = 0
 while True:
     try:
         offset = arq.tell()
         obj = pickle.load(arq)
         inscricao = obj.getInscricao()
         tree.insert(int(inscricao), offset)
-        i += 1
     except (EOFError):
         break
 
